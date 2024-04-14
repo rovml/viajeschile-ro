7b75a462ed6bec06f85d26d5996f7e17c818057e
rovml <rociomiranda0397@gmail.com>
2024-04-14 19:27:25 -0400

    js y cambios generales

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+    $(".segdes1").click(function() {
+     $(".segdes1").toggle()
+    });
+  });
+
+  $(document).ready(function() {
+    $(".segdes2").click(function() {
+     $(".segdes2").toggle()
+    });
+  });
+
+  $(document).ready(function() {
+    $(".segdes3").click(function() {
+     $(".segdes3").toggle()
+    });
+  });
+
+  $(document).ready(function() {
+    $(".segdes4").click(function() {
+     $(".segdes4").toggle()
+    });
+  });
